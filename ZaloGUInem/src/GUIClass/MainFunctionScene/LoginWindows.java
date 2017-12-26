806df80ec7b3bc95ddb9d75dec76bbe8b3fa0015
Nguyen Duc Manh <manhndgch16236@fpt.edu.vn>
2017-12-26 17:22:43 +0700

    manh-final

---
+++ ZaloGUInem/src/GUIClass/MainFunctionScene/LoginWindows.java
@@ -0,0 +1,40 @@
+package GUIClass.MainFunctionScene;
+
+import DataObject.Initialize_Zalo_Subject.ZaloOAAccess;
+import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginWindows implements Initializable{
+    public JFXButton LoginBtn;
+    public Label UsernameLabel;
+    public AnchorPane thisAnchorPane;
+
+
+    /*
+    Loading Username from Zalo OA
+     */
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        String name = "3093468968868500357";
+        UsernameLabel.setText(name);
+    }
+
+    /*
+    Login button. On click Action.
+     */
+    @FXML
+    protected void LoginProceed() throws IOException, URISyntaxException {
+        AnchorPane mainfucnt = FXMLLoader.load(getClass().getResource("MainFunctionWindows.fxml").toURI().toURL());
+        thisAnchorPane.getChildren().setAll(mainfucnt);
+    }
+
+}
